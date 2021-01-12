099e1455b6a4b6eee42aa5a64d793728f5a39d43
Victor Fomin <fominvic81@fominvic81.local>
2021-01-12 11:14:35 +0200

    Add method "off" to Events class

--- src/common/Events.js
+++ src/common/Events.js
@@ -1,22 +1,42 @@
-
+import { Common } from './Common';
 
 export class Events {
 
     constructor () {
         this.events = new Map();
+        this.namesByIds = new Map();
     }
 
     on (name, callback) {
-        if (!this.events.get(name)) {
-            this.events.set(name, []);
+        const id = Common.nextId();
+        const callbackId = Common.nextId();
+
+        this.namesByIds.set(id, [name, callbackId]);
+
+        let a = this.events.get(name);
+        if (!a) {
+            a = new Map();
+            this.events.set(name, a);
         }
-        this.events.get(name).push(callback);
+        a.set(callbackId, callback);
+
+        return id;
+    }
+
+    off (id) {
+        const a = this.namesByIds.get(id);
+        if (!a) return false;
+
+        const name = a[0];
+        const callbackId = a[1];
+
+        return this.events.get(name).delete(callbackId);
     }
 
     trigger (name, args = []) {
         const event = this.events.get(name);
         if (!event) return;
-        for (const callback of event) {
+        for (const callback of event.values()) {
             callback(...args);
         }
     }
